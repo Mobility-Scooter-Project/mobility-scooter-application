1966152e76a70b64678ae013d06ed785ab380db6
CrystarRay <crystar151129@gmail.com>
2023-07-05 10:42:52 -0700

    add all project

---
+++ TheMobilityscooterApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "The_Mobility_scooter_App"
+include(":androidApp")
+include(":shared")
