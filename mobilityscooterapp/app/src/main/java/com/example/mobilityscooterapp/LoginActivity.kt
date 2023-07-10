2598f87088cee9ed57c8954943025c7c67fd9958
CrystarRay <crystar151129@gmail.com>
2023-07-09 21:55:43 -0700

    finish all the basic LoginActivity, right now user can login

--- mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/LoginActivity.kt
+++ mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/LoginActivity.kt
@@ -1,11 +1,46 @@
 package com.example.mobilityscooterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Sms.Intents
+import android.widget.Toast
+import com.example.mobilityscooterapp.databinding.ActivityLoginBinding
+import com.example.mobilityscooterapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else{
+                        Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.createAccountButton.setOnClickListener {
+            val create_an_account_Intent = Intent(this, SignupActivity::class.java)
+            startActivity(create_an_account_Intent)
+        }
     }
 }
