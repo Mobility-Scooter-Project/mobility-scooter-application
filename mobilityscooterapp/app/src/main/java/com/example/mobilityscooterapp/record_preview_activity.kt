4e6dae824f933b679e25a387878afb2eddaf44d7
CrystarRay <crystar151129@gmail.com>
2023-07-11 11:47:46 -0700

    get permission from user

--- mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/record_preview_activity.kt
+++ mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/record_preview_activity.kt
@@ -1,5 +1,6 @@
 package com.example.mobilityscooterapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,34 @@ class record_preview_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_preview)
+        get_permissions()
+
+    }
+    fun get_permissions(){
+        var permissionLst = mutableListOf<String>()
+
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            permissionLst.add(android.Manifest.permission.CAMERA)
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            permissionLst.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            permissionLst.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if(permissionLst.size > 0){
+            requestPermissions(permissionLst.toTypedArray(), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.forEach {
+            if(it != PackageManager.PERMISSION_GRANTED){
+                get_permissions()
+            }
+        }
     }
 }
