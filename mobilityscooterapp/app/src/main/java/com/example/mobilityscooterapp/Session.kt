03006080950a899eb72432493e640c5d49676598
CrystarRay <crystar151129@gmail.com>
2023-07-20 00:01:43 -0700

    update the realtime data page for user

---
+++ mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/Session.kt
@@ -0,0 +1,11 @@
+package com.example.mobilityscooterapp
+
+
+
+data class Session(
+    val date: String? = null,
+    val startTime: String? = null,
+    val sessionLength: String? = null,
+    val videoUrl: String? = null,
+    val thumbnail_url: String? = null
+)
