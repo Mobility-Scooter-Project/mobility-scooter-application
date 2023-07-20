dbf294a0ed3bdc3c4e6696086233830a5448c062
CrystarRay <crystar151129@gmail.com>
2023-07-20 00:02:38 -0700

    add a new page for detail view

--- mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/session_history_activity.kt
+++ mobilityscooterapp/app/src/main/java/com/example/mobilityscooterapp/session_history_activity.kt
@@ -1,11 +1,55 @@
 package com.example.mobilityscooterapp
-
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobilityscooterapp.databinding.ActivitySessionHistoryBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class session_history_activity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySessionHistoryBinding
+    private lateinit var adapter: SessionAdapter
+    private val sessionList = mutableListOf<Session>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_session_history)
+        binding = ActivitySessionHistoryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        adapter = SessionAdapter(sessionList, this)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        fetchSessions()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun fetchSessions() {
+        val db = Firebase.firestore
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        db.collection("users").document(userId!!).collection("sessions")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val session = document.toObject(Session::class.java)
+                    sessionList.add(session)
+                }
+                // update adapter
+                adapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
     }
 }
