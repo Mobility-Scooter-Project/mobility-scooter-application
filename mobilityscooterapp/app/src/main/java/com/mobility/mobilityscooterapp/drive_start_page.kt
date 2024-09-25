3aaa41cbeff0cc55b879c197d79e6d6be23de9cd
AlvanZ <alvanzhuang625@gmail.com>
2024-09-24 23:24:19 -0700

    separate perms for optimization

--- mobilityscooterapp/app/src/main/java/com/mobility/mobilityscooterapp/drive_start_page.kt
+++ mobilityscooterapp/app/src/main/java/com/mobility/mobilityscooterapp/drive_start_page.kt
@@ -9,12 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class drive_start_page : Fragment() {
-
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,30 +28,58 @@ class drive_start_page : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val homeButton = view.findViewById<TextView>(R.id.home)
-//        homeButton?.setOnClickListener {
-//            findNavController().navigate(R.id.action_drive_start_page_to_homeFragment)
-//        }
-//
-//        val analyticsButton = view.findViewById<TextView>(R.id.analytics_button)
-//        analyticsButton?.setOnClickListener {
-//            findNavController().navigate(R.id.action_drive_start_page_to_analytic_start_page)
-//        }
-//
-//        val messageButton = view.findViewById<TextView>(R.id.button4)
-//        messageButton?.setOnClickListener {
-//            findNavController().navigate(R.id.action_drive_start_page_to_messeges_page)
-//        }
-//
-//        val startButton = view.findViewById<Button>(R.id.start_record_button)
-//        startButton?.setOnClickListener {
-//            findNavController().navigate(R.id.action_drive_start_page_to_record_preview_activity)
-//        }
+        val homeButton = view.findViewById<TextView>(R.id.home)
+        homeButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_drive_start_page_to_homeFragment)
+        }
 
-        //get_permissions()
+        val analyticsButton = view.findViewById<TextView>(R.id.analytics_button)
+        analyticsButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_drive_start_page_to_analytic_start_page)
+        }
+
+        val messageButton = view.findViewById<TextView>(R.id.button4)
+        messageButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_drive_start_page_to_messeges_page)
+        }
+
+        val startButton = view.findViewById<Button>(R.id.start_record_button)
+        startButton?.setOnClickListener {
+                if(cameraPermissions() && writePermissions()){
+                    findNavController().navigate(R.id.action_drive_start_page_to_record_preview_activity)
+                }
+                else{
+                    if(cameraPermissions()==false){
+                        requestCameraPermission()
+                     }
+                    if(writePermissions()==false){
+                        requestWritePermissions()
+                    }
+                }
+
+        }
+//        get_permissions()
+    }
+    private fun cameraPermissions():Boolean{
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+    private fun writePermissions(): Boolean{
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
     }
 
-    fun get_permissions() {
+    private fun readPermissions(): Boolean{
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+    fun get_permissions(){
         var permissionLst = mutableListOf<String>()
 
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
@@ -63,6 +92,7 @@ class drive_start_page : Fragment() {
         if (permissionLst.size > 0) {
             requestPermissions(permissionLst.toTypedArray(), 101)
         }
+//
     }
 
     override fun onRequestPermissionsResult(
@@ -77,4 +107,23 @@ class drive_start_page : Fragment() {
             }
         }
     }
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 1001)
+        }
+    }
+    private fun requestWritePermissions() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1002)
+        }
+    }
+    private fun requestReadExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1003)
+        }
+    }
+
 }
